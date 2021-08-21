092e7d5e12062144507202b6703efc227fdae646
Arthur-cmd256 <arthur.nascimento2020@outlook.com>
2021-08-21 18:20:12 -0300

    Resolução do exercicio adicionado ao arquivo

--- PrimeirosCem.java
+++ PrimeirosCem.java
@@ -2,7 +2,50 @@ public class PrimeirosCem{
 
     public static void main(String[] args) {
         
+        int n;
         
+        for (n = 0 ; n < 101; n++){
+
+            System.out.print(n);
+            if (n < 100) {
+                System.out.print(" - ");
+            }
+            else{
+                System.out.println(" ");
+            }
+        }
+
+
+
+        n = 0;
+        while (n < 101){
+
+            System.out.print(n);
+            if (n < 100) {
+                System.out.print(" - ");
+            }
+            else{
+                System.out.println(" ");
+            }
+            n++;
+
+        }
+
+
+
+        n = 0;
+        do{
+
+            System.out.print(n);
+            if (n < 100) {
+                System.out.print(" - ");
+            }
+            else{
+                System.out.println(" ");
+            }
+            n++;
+
+        }while(n < 101);
 
     }
 
