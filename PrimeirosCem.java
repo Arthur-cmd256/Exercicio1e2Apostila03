54608a67ab0e95debbb9631acc8e67160eb39fdc
Arthur-cmd256 <arthur.nascimento2020@outlook.com>
2021-08-21 18:39:34 -0300

    Adicionando a resolução do exercicio 2 da apostila 03

--- PrimeirosCem.java
+++ PrimeirosCem.java
@@ -47,6 +47,46 @@ public class PrimeirosCem{
 
         }while(n < 101);
 
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------");
+
+        for (n = 100 ; n >= 0; n--){
+
+            System.out.print(n);
+            if (n >= 1) {
+                System.out.print(" - ");
+            }
+            else{
+                System.out.println(" ");
+            }
+        }
+
+        n = 100;
+        while (n >= 0){
+
+            System.out.print(n);
+            if (n >= 1) {
+                System.out.print(" - ");
+            }
+            else{
+                System.out.println(" ");
+            }
+            n--;
+
+        }
+
+        n = 100;
+        do{
+
+            System.out.print(n);
+            if (n >= 1) {
+                System.out.print(" - ");
+            }
+            else{
+                System.out.println(" ");
+            }
+            n--;
+
+        }while(n >= 0);
     }
 
 }
